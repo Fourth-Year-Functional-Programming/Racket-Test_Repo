10f471c248897abfb9316e52c887ae65708eea27
Christy <christymadden80@gmail.com>
2017-02-09 16:32:53 +0000

    Changed test file

--- test.rkt
+++ test.rkt
@@ -1,5 +1,14 @@
 #lang racket
 
-(define add (+ 3 5))
+;; A comment.
 
-(define subtract (- 5 1))
+(define (plus1 x)
+  (add1 x))
+
+(define (minus1 x)
+  (sub1 x))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (plus1 1) 2)
+  (check-equal? (minus1 1) 0))
